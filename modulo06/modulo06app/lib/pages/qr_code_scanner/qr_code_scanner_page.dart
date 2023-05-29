b6b1491dfa5945661fe5b7ad9f5197c9667aebde
Romildo Paiter <romildo.paiter@amedigital.com>
2023-05-29 19:32:08 -0300

    Incluindo o QR Code Scanner

---
+++ modulo06/modulo06app/lib/pages/qr_code_scanner/qr_code_scanner_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QrCodeScannerPage extends StatefulWidget {
+  const QrCodeScannerPage({super.key});
+
+  @override
+  State<QrCodeScannerPage> createState() => _QrCodeScannerPageState();
+}
+
+class _QrCodeScannerPageState extends State<QrCodeScannerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
