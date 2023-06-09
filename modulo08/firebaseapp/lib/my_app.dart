8c946f6d34ede439201cf8bf5805f986c2b96a0a
Romildo Paiter <romildopaiter@gmail.com>
2023-06-09 14:43:41 -0300

    Configuraçoes para funcionar o Firebase

---
+++ modulo08/firebaseapp/lib/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:firebaseapp/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
