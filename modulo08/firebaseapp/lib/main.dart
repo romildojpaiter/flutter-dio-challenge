5d268b21514fabd276e7ab5bad301133d8f5df72
Romildo Paiter <romildopaiter@gmail.com>
2023-06-13 21:58:15 -0300

    Incluindo o remote_config e definindo algumas chaves

--- modulo08/firebaseapp/lib/main.dart
+++ modulo08/firebaseapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:firebaseapp/infraestrutura/firebase_conn.dart';
 import 'package:firebaseapp/infraestrutura/impl/firebase_conn_infra.dart';
 import 'package:firebaseapp/repository/impl/mensagem_repository_impl.dart';
@@ -19,8 +20,10 @@ void setupGetIt() {
   getIt.registerSingleton<UsuarioController>(UsuarioController());
 
   // Repositories
-  getIt.registerSingleton<TarefaRepository>(TarefaRespositoryImpl(firebaseConn: getIt<FirebaseConn>()));
-  getIt.registerSingleton<MensagemRepository>(MensagemRepositoryImpl(firebaseConn: getIt<FirebaseConn>()));
+  getIt.registerSingleton<TarefaRepository>(
+      TarefaRespositoryImpl(firebaseConn: getIt<FirebaseConn>()));
+  getIt.registerSingleton<MensagemRepository>(
+      MensagemRepositoryImpl(firebaseConn: getIt<FirebaseConn>()));
 
   // Alternatively you could write it if you don't like global variables
   // GetIt.I.registerSingleton<AppModel>(AppModel());
@@ -32,6 +35,20 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(minutes: 1),
+    minimumFetchInterval: const Duration(hours: 1),
+  ));
+  await remoteConfig.setDefaults(const {
+    "example_param_1": 42,
+    "example_param_2": 3.14159,
+    "example_param_3": true,
+    "example_param_4": "Hello, world!",
+  });
+  // Teste recuperando Remote-Config
+  await remoteConfig.fetchAndActivate();
+  print(remoteConfig.getString("example_param_4"));
 
   await dotenv.load(fileName: ".env");
 
